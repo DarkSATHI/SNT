b655e5f9935682d2abaaba1c0d4866805e42a609
DarkSATHI <tsautiere@aol.com>
2022-06-11 18:23:09 +0200

    Update 1_2️⃣_Internet.py

--- pages/1_2️⃣_Internet.py
+++ pages/1_2️⃣_Internet.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-image = Image.open('./assets/snt.png')
+image = Image.open('../assets/snt.png')
 
 st.image(image, width=250, caption='https://wall.alphacoders.com/')
 
